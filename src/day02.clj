bad1b2cba3064ffce461c7a21bac83b10dc0a1ea
mcarfagno <carfagnomarcello@gmail.com>
2023-12-03 01:14:24 +0000

    WIP: day2

--- src/day02.clj
+++ src/day02.clj
@@ -5,28 +5,36 @@
 
 (defn read-input [file] (str/split-lines (slurp file)))
 
-(defn to-cubes [x] (assoc {"red" 0, "green" 0, "blue" 0} x))
-
-(defn parse-cubes
-  [x]
-  (for [draw (str/split x #";")]
-    ((fn [x] (map (fn [a] (reverse (str/split a #" "))) (str/split x #",")))
-      draw)))
-
 (defn get-games
   [x]
   (into (sorted-map)
         (map (fn [a]
                (let [[game-number game-results] (str/split a #":")]
                  [(read-string (str/replace game-number #"Game " ""))
-                  (parse-cubes game-results)]))
+                  (str/split game-results #";")]))
           x)))
 
+(defn get-max-reds
+  [a]
+  (apply max
+    (map read-string (map second (re-seq #"(\d+)[^\d]+red" (str/join a))))))
 
+(defn get-max-blue
+  [a]
+  (apply max
+    (map read-string (map second (re-seq #"(\d+)[^\d]+blue" (str/join a))))))
+(defn get-max-green
+  [a]
+  (apply max
+    (map read-string (map second (re-seq #"(\d+)[^\d]+green" (str/join a))))))
 (defn solve1
   [input]
   (->> input
        (get-games)
-       (println)))
+       (filter (fn [[k v]] (<= (get-max-reds v) 12)))
+       (filter (fn [[k v]] (<= (get-max-green v) 13)))
+       (filter (fn [[k v]] (<= (get-max-blue v) 14)))
+       (keys)
+       (reduce +)))
 
-(solve1 (read-input "../test/day02.txt"))
+(println (solve1 (read-input "../test/day02.txt")))
