bf4d8bff55feb6e553610e2acc1a445a550d3a05
mcarfagno <carfagnomarcello@gmail.com>
2023-12-03 17:46:47 +0000

    WIP: day3

--- src/day03.clj
+++ src/day03.clj
@@ -1,25 +1,49 @@
 (ns aoc)
 (require '[clojure.string :as str])
 
+(def input-file "../test/day03.txt")
 (defn read-input [file] (str/split-lines (slurp file)))
 (defn parse-schematic [x] (for [line x] (str/split line #"")))
-(defn get-symbols-positions
+(defn get-from-schematic [x [col row]] (nth (nth x col) row))
+
+(defn get-symbols-idx
   [x]
-  (println (count x))
-  (println (count (first x)))
   (for [col (range (count x))
         row (range (count (first x)))
-        :when (re-matches #"[$]|[#]|[*]|[-]|[*]|[+]" (nth (nth x col) row))]
+        :when (re-matches #"[$]|[#]|[*]|[-]|[*]|[+]"
+                          (get-from-schematic x [col row]))]
     [col row]))
+
+(defn get-numbers-idx
+  [x]
+  (for [col (range (count x))
+        row (range (count (first x)))
+        :when (re-matches #"\d" (get-from-schematic x [col row]))]
+    [col row]))
+
+(defn surrounding-idx
+  [[a b]]
+  [[(inc a) b] [(dec a) b] [a (inc b)] [a (dec b)] [(inc a) (inc b)]
+   [(dec a) (inc b)] [(inc a) (dec b)] [(dec a) (dec b)]])
+
+
+;; set of valid schematics positions for digits to be in
+;; aka set of all idx adjacent to a symbol
+(def valid-positions
+  (set (reduce into
+         []
+         (map surrounding-idx
+           (get-symbols-idx (parse-schematic (read-input input-file)))))))
+
+; create seq containing indices of digits close to a symbol
+(defn candidates [x] (filter (fn [a] (contains? valid-positions a)) x))
 
 (defn part1
   [input]
   (->> input
        (parse-schematic)
-       (get-symbols-positions)
-       (println)
-       ;((fn [x] (re-seq #"\d+|[$]|[#]|[*]|[-]|[*]|[+]" x))) ;ty regex101
-  ))
+       (get-numbers-idx)
+       (candidates))) ;;sequences of indices where a valid number is located
 
 
-(println (part1 (read-input "../test/day03.txt")))
+(println (part1 (read-input input-file)))
