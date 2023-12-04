e97806e8cd7dfd824391ce36de3f86e80d208e0f
mcarfagno <carfagnomarcello@gmail.com>
2023-12-04 21:09:27 +0000

    tidy up

--- src/day04.clj
+++ src/day04.clj
@@ -4,22 +4,29 @@
 
 (defn read-input [file] (str/split-lines (slurp file)))
 
-;  (map (fn [[winning hand]] [(set (read-string winning))
-;                             (set (read-string hand))])
 (defn parse-card
   [card]
   (map #(set (filter some? (map parse-long (str/split % #" "))))
     (str/split (str/replace card #"Card \d[:]" "") #"[|]")))
+
 (defn exp [x n] (reduce * (repeat n x)))
+
 (defn solve1
   [input]
   (->> input
        (map parse-card)
        (map (fn [[hand winning]]
               (count (clojure.set/intersection hand winning))))
-       (filter pos?)
-       (map #(exp 2 (dec %)))
+       (map #(if (pos? %) (exp 2 (dec %)) 0))
        (reduce +)))
 
-
+(defn solve2
+  [input]
+  (->> input
+       (map parse-card)
+       (map (fn [[hand winning]]
+              (count (clojure.set/intersection hand winning))))
+       (map #(if (pos? %) (exp 2 (dec %)) 0))
+       (reduce +)))
 (println (solve1 (read-input "../input/day04.txt")))
+(println (solve2 (read-input "../input/day04.txt")))
