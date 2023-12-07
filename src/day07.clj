35a6286e8ad6d3701bca5a285bd8e5f07ae19538
mcarfagno <carfagnomarcello@gmail.com>
2023-12-07 20:22:46 +0000

    day7 part1

--- src/day07.clj
+++ src/day07.clj
@@ -22,22 +22,57 @@
 (defn rank
   [x]
   (case x
-    [5 0] 7 ;;Five of a kind
-    [4 1] 6 ;;Four of a kind
-    [3 2] 5 ;;Full house
-    [3 1] 4 ;;Three of a kind
-    [2 2] 3 ;;Two pair
-    [2 1] 2 ;;One pair
-    [1 1] 1));;High card
+    [5 0] 0 ;;Five of a kind
+    [4 1] 1 ;;Four of a kind
+    [3 2] 2 ;;Full house
+    [3 1] 3 ;;Three of a kind
+    [2 2] 4 ;;Two pair
+    [2 1] 5 ;;One pair
+    [1 1] 6));;High card
 
-;(group-by #(get % :category) [{:category "a" :id 1}
-;                                     {:category "a" :id 2}
-;                                     {:category "b" :id 3}]
+(defn card-priority
+  [x]
+  (case (str x)
+    "A" 0
+    "K" 1
+    "Q" 2
+    "J" 3
+    "T" 4
+    "9" 5
+    "8" 6
+    "7" 7
+    "6" 8
+    "5" 9
+    "4" 10
+    "3" 11
+    "2" 12))
+
+(defn has-priority?
+  [x y]
+  (let [a (get x :hand)
+        b (get y :hand)]
+    (loop [idx 0]
+      (case (compare (card-priority (get a idx)) (card-priority (get b idx)))
+        -1 true
+        1 false
+        0 (recur (inc idx))))))
+
+(defn total_winnings
+  [x]
+  (reduce +
+    (for [i (range (count x))]
+      (let [rank (- (count x) i)
+            bid (get (nth x i) :bid)]
+        (* rank (parse-long bid))))))
+
 (defn solve1
   [input]
   (->> input
        (map get-hand)
-       (group-by #(rank (hand-type %)))
-       (println)))
+       (group-by #(rank (hand-type %))) ;initial "sorting", group by rank
+       (into (sorted-map)) ; make sure its sorted
+       (map #(sort has-priority? (second %))) ; sort the groups
+       (flatten) ; make 1-d
+       (total_winnings)))
 
-(println (solve1 (read-input "../test/day07.txt")))
+(println (solve1 (read-input "../input/day07.txt")))
