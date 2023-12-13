b387db146b8801fcab3b9fb08a2020c7d5d1871a
mcarfagno <carfagnomarcello@gmail.com>
2023-12-13 14:35:43 +0000

    wip day08p2

--- src/day08.clj
+++ src/day08.clj
@@ -14,8 +14,8 @@
   [current next-step]
   (let [{curr-node :node, step :step, nodes :map} current
         bifurcarion (first (filter #(= curr-node (:node %)) nodes))]
-    (case curr-node
-      "ZZZ" (reduced current)
+    (if (= curr-node "ZZZ")
+      (reduced current)
       {:node (if (= next-step "R") (:right bifurcarion) (:left bifurcarion)),
        :step (inc step),
        :map nodes})))
@@ -32,4 +32,43 @@
        (walk-map)
        (:step)))
 
+;; part 2
+(defn get-nodes
+  [x regex]
+  (set (filter #(re-matches regex %) (for [y x] (get y :node)))))
+
+(defn step-all-nodes
+  [curr-nodes move nodes]
+  (set (for [n curr-nodes
+             :let [bifurcarion (first (filter #(= n (:node %)) nodes))]]
+         (if (= move "R") (:right bifurcarion) (:left bifurcarion)))))
+
+(defn next-nodes
+  [current next-step]
+  (let [{curr-nodes :nodes, targets :targets, step :step, nodes :map} current]
+    (if (every? curr-nodes targets)
+      (reduced current)
+      {:nodes (step-all-nodes curr-nodes next-step nodes),
+       :step (inc step),
+       :targets targets,
+       :map nodes})))
+
+(defn walk-map-multi
+  [[instructions nodes]]
+  (let [inst-seq (cycle (str/split instructions #""))
+        src (get-nodes nodes #"..[A]")
+        trg (get-nodes nodes #"..[Z]")
+        current {:nodes src, :targets trg, :step 0, :map nodes}]
+    (println src)
+    (println trg)
+    (reduce next-nodes current inst-seq)))
+
+
+(defn solve2
+  [input]
+  (->> input
+       (walk-map-multi)
+       (:step)))
+
 (println (solve1 (read-input "../input/day08.txt")))
+(println (solve2 (read-input "../input/day08.txt")))
