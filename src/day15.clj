53c0746c9cbd1f776726159fa3a9ed68732ad843
mcarfagno <carfagnomarcello@gmail.com>
2023-12-16 10:14:27 +0000

    day15 p1

--- src/day15.clj
+++ src/day15.clj
@@ -1,10 +1,16 @@
 (ns aoc)
 (require '[clojure.string :as str])
 
-(defn read-input [file] (str/split-lines (slurp file)))
+(defn read-input [file] (str/split (str/replace (slurp file) #"\n" "") #","))
 
 (defn str-to-ASCII [x] (map int (seq (char-array x))))
 (defn HASH [x] (reduce (fn [a b] (rem (* (+ a b) 17) 256)) 0 x))
-(println (str-to-ASCII "HASH"))
-(println (HASH (str-to-ASCII "HASH")))
 
+(defn solve1
+  [input]
+  (->> input
+       (map str-to-ASCII)
+       (map HASH)
+       (reduce +)))
+
+(println (solve1 (read-input "../input/day15.txt")))
