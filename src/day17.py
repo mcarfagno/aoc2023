2868a6c653da1699b4df92e51e792dea2b0c92b3
mcarfagno <carfagnomarcello@gmail.com>
2023-12-17 19:43:11 +0000

    day17 fix neighbours

--- src/day17.py
+++ src/day17.py
@@ -2,11 +2,19 @@ import heapq
 from enum import Enum
 
 
-class direction(Enum):
-    RIGHT = (+1, 0)
-    LEFT = (-1, 0)
-    UP = (0, +1)
-    DOWN = (0, -1)
+direction = {
+    ">": (+1, 0),
+    "<": (-1, 0),
+    "^": (0, +1),
+    "v": (0, -1),
+}
+
+turns = {
+    ">": (">", "v", "^"),
+    "<": ("<", "v", "^"),
+    "^": ("<", ">", "^"),
+    "v": ("<", "v", ">"),
+}
 
 
 class WeightedGrid:
@@ -26,53 +34,21 @@ class WeightedGrid:
         (x, y, _, _) = id
         return 0 <= x < self.width and 0 <= y < self.height
 
-    def next_dir(self, d, n):
-        if n < 3:
-            match d:
-                case direction.RIGHT:
-                    return (
-                        (direction.RIGHT, n + 1),
-                        (direction.DOWN, 0),
-                        (direction.UP, 0),
-                    )
-                case direction.LEFT:
-                    return (
-                        (direction.LEFT, n + 1),
-                        (direction.DOWN, 0),
-                        (direction.UP, 0),
-                    )
-                case direction.DOWN:
-                    return (
-                        (direction.LEFT, 0),
-                        (direction.DOWN, n + 1),
-                        (direction.RIGHT, 0),
-                    )
-                case direction.UP:
-                    return (
-                        (direction.LEFT, 0),
-                        (direction.UP, n + 1),
-                        (direction.RIGHT, 0),
-                    )
-
-        else:
-            match d:
-                case direction.RIGHT:
-                    return ((direction.DOWN, 0), (direction.UP, 0))
-                case direction.LEFT:
-                    return ((direction.DOWN, 0), (direction.UP, 0))
-                case direction.DOWN:
-                    return ((direction.LEFT, 0), (direction.RIGHT, 0))
-                case direction.UP:
-                    return ((direction.LEFT, 0), (direction.RIGHT, 0))
+    def in_run(self, id):
+        (_, _, _, i) = id
+        return 0 <= i < 3
 
     def neighbors(self, id):
         (x, y, d, n) = id
 
         neighbors = []
-        for dn, nn in self.next_dir(d, n):
-            (xs, ys) = dn.value
+        for dn in turns[d]:
+            nn = n + 1 if (dn == d) else 0
+            (xs, ys) = direction[dn]
             neighbors.append((x + xs, y + ys, dn, nn))
-        return filter(self.in_bounds, neighbors)
+        neighbors = filter(self.in_bounds, neighbors)
+        neighbors = filter(self.in_run, neighbors)
+        return neighbors
 
 
 def heuristic(a, b):
@@ -85,29 +61,30 @@ def a_star_search(graph, start, goal):
     frontier = []
     heapq.heappush(frontier, (0, start))
     cost_so_far = {}
-    cost_so_far[start[0:2]] = 0
+    cost_so_far[start] = 0
     while frontier:
         _, current = heapq.heappop(frontier)
         # print(f"Visiting: {current}")
         if (current[0:2]) == goal:
+            print("found goal")
             break
 
         for next in graph.neighbors(current):
-            new_cost = cost_so_far[current[0:2]] + graph.cost(current, next)
-            if next[0:2] not in cost_so_far or new_cost < cost_so_far[next[0:2]]:
-                cost_so_far[next[0:2]] = new_cost
-                priority = new_cost + heuristic(next[0:2], goal)
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if new_cost < cost_so_far.get(next, float("inf")):
+                cost_so_far[next] = new_cost
+                priority = new_cost  # + heuristic(next[0:2], goal)
                 heapq.heappush(frontier, (priority, next))
-    return cost_so_far[current[0:2]]
+    return cost_so_far[current]
 
 
-with open("../test/day17.txt") as f:
+with open("../input/day17.txt") as f:
     grid = [line.strip() for line in f.readlines()]
     graph = WeightedGrid(grid)
     h = len(grid)
     w = len(grid[0])
 
 # part 1
-start = (0, 0, direction.RIGHT, 0)
+start = (0, 0, ">", 0)
 cost = a_star_search(graph, start, (w - 1, h - 1))
 print(cost)
