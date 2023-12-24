8bca6de62dcd3ffc4e164f427e454fdcf680b6c2
mcarfagno <carfagnomarcello@gmail.com>
2023-12-24 15:06:43 +0000

    day24p1

--- src/day24.py
+++ src/day24.py
@@ -4,6 +4,10 @@ from itertools import combinations
 MIN = 200000000000000
 MAX = 400000000000000
 file = "../input/day24.txt"
+
+# MIN = 7
+# MAX = 27
+# file = "../test/day24.txt"
 hailstones = [h for h in open(file).readlines()]
 
 # fotrmat [x,y,z,dx,dy,dz]
@@ -12,9 +16,7 @@ hailstones = list(
         lambda x: list(
             map(
                 int,
-                re.findall(
-                    r"(\d+)..(\d+)..(\d+).[@].([-]?\d+)..([-]?\d+)..([-]?\d+)", x
-                )[0],
+                re.findall(r"(\d+)..(\d+)..(\d+).[@].(.?\d+)..(.?\d+)..(.?\d+)", x)[0],
             )
         ),
         hailstones,
@@ -39,20 +41,33 @@ def line_intersection(eq1, eq2):
 def intersects(h1, h2):
     (x1, y1, _, dx1, dy1, _) = h1
     (x2, y2, _, dx2, dy2, _) = h2
-    px = line_intersection((dx1, x1), (dx2, x2))
-    py = line_intersection((dy1, y1), (dy2, y2))
 
-    # parallel or intersected in the past
-    if any([px is None, py is None]):
-        return 0
-    if any([px[0] <= 0, py[0] <= 0]):
-        return 0
-    # print(f"hailstones intersects in {px[1]}{py[1]}")
-    if all([px[1] >= MIN, px[1] <= MAX, py[1] >= MIN, py[1] <= MAX]):
-        return 1
+    # pos and vel to y = ax+b
+    a1 = dy1 / dx1
+    b1 = y1 - a1 * x1
 
-    # intersects outside
-    return 0
+    a2 = dy2 / dx2
+    b2 = y2 - a2 * x2
+
+    p = line_intersection((a1, b1), (a2, b2))
+
+    # print(f"hailstones {h1} {h2} -> {p}")
+    if p is None:
+        return 0
+
+    # was the hailstone in that point in the past?
+    # x = x0 + dx*t -> t = (x-x0)/dx
+    if any(
+        [
+            (p[0] - x1) / dx1 <= 0,
+            (p[1] - y1) / dy1 <= 0,
+            (p[0] - x2) / dx2 <= 0,
+            (p[1] - y2) / dy2 <= 0,
+        ]
+    ):
+        return 0
+
+    return 1 if all([MIN <= p[0] <= MAX, MIN <= p[1] <= MAX]) else 0
 
 
 def part1():
